6a1cfc9e2fda6d3fc955224258e7814d5447f23e
pgd <rudeh1253@gmail.com>
2024-06-10 11:37:01 +0900

    feat: 지도에 건물 마커 찍기 구현
    Related to: #10

--- src/pages/map/BMap.js
+++ src/pages/map/BMap.js
@@ -1,4 +1,5 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
+import $ from "jquery";
 
 const naver = window.naver;
 
@@ -9,17 +10,30 @@ export default function BMap() {
   useEffect(() => {
     const mapElement = document.getElementById("map");
     map = new naver.maps.Map(mapElement);
+    fetchBuildingMarkers(); // TODO: 구독건물보기하고 인기건물보기 중 뭐가 디폴트였더라?
   }, []);
   
   return (
     <>
-      <div id="map" style={{width: "400px", height: "400px"}}></div>
+      <div id="map" style={{width: "400px", height: "400px", cursor: "none"}}></div>
     </>
   )
 }
 
-const Liveliness = {
+/**
+ * @param {"SUB" | "POPULAR"} type 
+ */
+function fetchBuildingMarkers(type) {
+  // TODO
+}
 
+const liveliness = {
+  1: "#e03131",
+  2: "#f08c00",
+  3: "#ffd43b",
+  4: "#37b24d",
+  5: "#adb5bd",
+  6: "#212529"
 }
 
 /**
@@ -28,23 +42,40 @@ const Liveliness = {
  *   longitude: number;
  *   buildingName: string;
  *   livelistChatroom: {
- *     liveliness: Liveliness;
+ *     liveliness: number;
  *     chatroomName: string;
  *   };
  *   subscriptionProviders: string[];
- * }} content
+ * }} marker
  */
-function addMarker(content) {
-  const marker = new naver.maps.Marker({
-    position: new naver.maps.LatLng(content.latitude, content.longitude),
-    map: map
+function addBuildingMarker(marker) {
+  const contentHtmlText = `
+  <div style="display: flex; flex-direction: column; align-items: center; width: fit-content; height: fit-content; margin: 0px; padding: 0px">
+    <div style="text-align: center;">
+      <p>${marker.subscriptionProviders[0]}</p>
+      <p>${marker.livelistChatroom.chatroomName}</p>
+      <p>${marker.buildingName}</p>
+    </div>
+    <div style="display: flex; justify-content: center; align-items: center; ">
+      <img src="./image/marker.png" style="width: 40px; height: 50px; margin: 0px; padding: 0px" />
+    </div>
+  </div>
+  `
+
+  $(document).children().append($(contentHtmlText).addClass("temp"));
+  const width = $(".temp").width();
+  const height = $(".temp").height();
+  $(".temp").attr("width", width).attr("height", height);
+  const contentHtml = $(".temp").html();
+  $(document).find(".temp").remove();
+
+  new naver.maps.Marker({
+    position: new naver.maps.LatLng(marker.latitude, marker.longitude),
+    map: map,
+    icon: {
+        content: contentHtml,
+        size: new naver.maps.Size(width, height)
+
+    }
   });
-  
-  const infoWindow = new naver.maps.InfoWindow({
-    content,
-    borderWidth: 0,
-    disableAnchor: true,
-    backgroundColor: "transparent"
-  });
-  infoWindow.open(map, marker);
 }
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -18,7 +18,7 @@ import ChatRoomCreation from "../pages/Chat/ChatroomCreation";
 import Chatroom from "../pages/Chat/Chatroom";
 import MyChatroomList from "../pages/Chat/MyChatroomList";
 import BMap from "../pages/map/BMap";
-import Test from '../pages/Chat/Test';
+// import Test from '../pages/Chat/Test';
 
 const AppRoutes = () => {
   return (
@@ -41,7 +41,7 @@ const AppRoutes = () => {
             <Route path="chatroomCreation" element={<ChatRoomCreation />} />
             <Route path="chatroom" element={<Chatroom />} />
             <Route path="myChatroomList" element={<MyChatroomList />} />
-            <Route path="test" element={<Test/>}/>
+            {/* <Route path="test" element={<Test/>}/> */}
         </Route>
         <Route path="map" element={<BMap />} />
     </Routes>
