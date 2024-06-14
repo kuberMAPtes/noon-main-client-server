7fc39b2e4afd2d8ac00db98d9bed7f47bed21f1a
HyeonJun0527 <osumaniaddict527@gmail.com>
2024-06-14 19:25:45 +0900

    Refactor: axios
    Related To:#23

--- src/pages/member/function/memberAxios.js
+++ src/pages/member/function/memberAxios.js
@@ -1,29 +1,4 @@
 import axiosInstance from "../../../lib/axiosInstance";
-
-export const getMemberProfile = async (fromId, toId) => {
-    try{
-        console.log("fromId:",fromId);
-        console.log("toId:",toId);
-        const response = await axiosInstance.post(`/member/getMemberProfile`, {fromId, toId});
-        console.log(response);
-        return response.data.info;
-        // return null;
-    } catch (error) {
-        console.error('getMemberProfile error:',error);
-        throw error;
-    }
-};
-
-export const getMember = async (member) => {
-    try{
-        const response = await axiosInstance.post(`/member/getMember`,member);
-        return response.data;
-    } catch(error){
-        console.error('getMember error:',error);
-        console.log("그런 계정은 존재하지 않습니다.");
-        return null;
-    }
-};
 // 인증번호전송
 export const sendAuthentificationNumber = async (phoneNumber) => {
     try{
@@ -123,4 +98,78 @@ export const addMember = async (addMemberDto) => {
         throw error;
     }
 };
-//
+//비밀번호 변경
+export const updatePassword = async (memberId, password) => {
+    try {
+        const response = await axiosInstance.post(`/member/updatePassword/`,{memberId, password});
+    }catch(error){
+        console.error(`updatePassword error:`,error);
+        throw error;
+    }
+}
+//전화번호 변경
+//프로필 사진 변경
+//프로필 소개 변경
+//다정점수 변경
+//회원정보 변경 - 웬만하면 쓰지 말기
+export const updateMember = async (member) => {
+    try{
+        const response = await axiosInstance.post(`/member/updateMember/`,member);
+    }catch(error){
+        console.error(`updateMember error:`,error);
+        throw error;
+    }
+}
+//회원조회
+export const getMember = async (member) => {
+    try{
+        const response = await axiosInstance.post(`/member/getMember`,member);
+        return response.data;
+    } catch(error){
+        console.error('getMember error:',error);
+        console.log("그런 계정은 존재하지 않습니다.");
+        return null;
+    }
+};
+//회원프로필조회
+export const getMemberProfile = async (fromId, toId) => {
+    try{
+        console.log("fromId:",fromId);
+        console.log("toId:",toId);
+        const response = await axiosInstance.post(`/member/getMemberProfile`, {fromId, toId});
+        console.log(response);
+        return response.data.info;
+        // return null;
+    } catch (error) {
+        console.error('getMemberProfile error:',error);
+        throw error;
+    }
+};
+//회원 목록 조회
+export const listMember = async() => {
+    try{
+        const response = await axiosInstance.post(`/member/listMember`,{
+            
+        })
+    }catch(error){
+        console.error(`listMember error`, error);
+        throw error;
+    }
+}
+//회원 삭제
+export const deleteMember = async () =>{
+    try{
+        const response = await axiosInstance.post(`/member/deleteMember`,{
+            params : {memberId}
+        });
+
+    }catch(error){
+        console.error(`deleteMember error:`, error);
+        throw error;
+    }
+};
+//회원 관계 추가
+
+//회원 관계 목록 조회
+
+//회원 관계 삭제
