c66a7d5a2aaad0c5ad1d71443e30bdbaca8625a9
ellen310 <ellen10735@gmail.com>
2024-06-19 09:15:09 +0900

    feat : 빌딩 프로필 서버 데이터 연동

---
+++ src/pages/building/GetBuilding.js
@@ -0,0 +1,20 @@
+import BuildingInfo from './components/BuildingInfo';
+import TabNavigation from './components/TabNavigation';
+import Footer from '../../components/common/Footer';
+
+
+const Building = () => {
+  
+  return (
+    <div className="building-container">
+      
+    <BuildingInfo/>
+    <TabNavigation/>
+    <Footer/>
+
+    </div>
+    
+  );
+};
+
+export default Building;
