f6a8c836c964490d195615a1d8e1f75a86ac3390
WinterHana <haha920@naver.com>
2024-06-21 14:30:23 +0900

    feat : 상단바 조정
    - 상단바 내용 다 쳐내고 로그인한 회원 이름, 피드에 대한 차트만 남김

--- src/components/common/BasicNavbar.jsx
+++ src/components/common/BasicNavbar.jsx
@@ -1,37 +1,32 @@
 import React from 'react';
-// import { IoNotifications, IoSettingsSharp  } from "react-icons/io5";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import { useSelector } from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
+import Navigator from "../../pages/feed/util/Navigator.js";
 
 function BasicNavbar(args) {
+  const [searchParams] = useSearchParams();
+  const memberIdFromStore = useSelector((state) => state.auth.member.memberId);
+  const memberIdFromURL = searchParams.get('memberId');
+  const memberId = memberIdFromStore || memberIdFromURL || "로그인 안함";
+
+  const {goToMemberProfile} = Navigator();
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/">NoonSNS</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
+            <Nav.Link onClick={() => goToMemberProfile(memberId)}>{memberId}</Nav.Link>
             <Nav.Link href="/feed/chart">Feed Chart</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
           </Nav>
           <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link href="#deets">Bitcamp</Nav.Link>
             <Nav.Link eventKey={2} href="#memes">
-              Dank memes
+            KuberMaptes
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
