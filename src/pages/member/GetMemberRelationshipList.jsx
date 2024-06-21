9d5108a7cc83ba1e80eda13a827fff213ed369e6
HyeonJun0527 <osumaniaddict527@gmail.com>
2024-06-22 03:44:05 +0900

    Refactor:팔로잉리팩토링중
    Related To:#82

--- src/pages/member/GetMemberRelationshipList.jsx
+++ src/pages/member/GetMemberRelationshipList.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useInfiniteScrolls from "./component/common/useInfiniteScrolls";
 import useFetchMemberRelationshipList from "./component/common/useFetchMemberRelationshipList";
 import { useSelector } from "react-redux";
 import UseGetMemberRelationshipLists from "./component/memberRelationshipList/UseGetMemberRelationshipLists";
+import { initialBlockingList, initialFollowerList, initialFollowingList } from "./function/GetMemberRelationshipListUtil";
 
 // 팔로잉 팔로워 리스트임
 const GetMemberRelationshipList = () => {
@@ -24,6 +25,21 @@ const GetMemberRelationshipList = () => {
 
   // 무한 스크롤을 위한 커스텀 훅 사용
   const { lastElementRef } = useInfiniteScrolls(fetchMoreData);
+
+
+  
+  const [followingList, setFollowingList] = useState(initialFollowingList);
+  const [followerList, setFollowerList] = useState(initialFollowerList);
+  const [blockingList, setBlockingList] = useState(initialBlockingList);
+  const [activeTab, setActiveTab] = useState('following');
+
+  useEffect(()=> {
+    //memberRelationshipList에서 팔로잉, 팔로워, 차단 리스트를 만든다.
+    
+          {/* if(toId===memberRelationship.fromMember.memberId && memberRelationship.relationshipType==="FOLLOW")이면 팔로잉
+              if(toId===memberRelationship.toMember.memberId && memberRelationship.relationshipType==="FOLLOW")이면 팔로워
+              if(toId===memberRelationship.fromMember.memberId && memberRelationship.relationshipType==="BLOCK")이면 내가 차단한 목록 */}
+  },[memberRelationshipList]);
 
   // 서로 관계 확인 및 팔로우/언팔로우 버튼 생성 로직
   //서로관계확인해서 상대방과 내가 서로팔로우했으면 팔로우 취소하기 버튼
@@ -51,9 +67,6 @@ const GetMemberRelationshipList = () => {
           ref={memberRelationshipList.length === index + 1 ? lastElementRef : null}
         >
 
-          {/* if(toId===memberRelationship.fromMember.memberId && memberRelationship.relationshipType==="FOLLOW")이면 팔로잉
-              if(toId===memberRelationship.toMember.memberId && memberRelationship.relationshipType==="FOLLOW")이면 팔로워
-              if(toId===memberRelationship.fromMember.memberId && memberRelationship.relationshipType==="BLOCK")이면 내가 차단한 목록 */}
           {memberRelationship.fromMember.nickname}프럼닉네임
           {memberRelationship.toMember.nickname}투닉네임
           {memberRelationship.relationshipType}팔로우타입
