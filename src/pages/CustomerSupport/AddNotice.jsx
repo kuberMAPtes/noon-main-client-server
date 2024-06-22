e56c6e683c83ba840d96828733e36db7e1027774
ellen310 <ellen10735@gmail.com>
2024-06-22 15:40:14 +0900

    refactor : notice - Multipart upload and WYSIWYG

--- src/pages/CustomerSupport/AddNotice.jsx
+++ src/pages/CustomerSupport/AddNotice.jsx
@@ -1,20 +1,18 @@
-import React, { useState, useRef, useEffect, useMemo } from 'react';
+import React, { useState, useRef, useMemo } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import '../CustomerSupport/css/text-editor.css';
 import axiosInstance from '../../lib/axiosInstance';
+import { useSelector } from 'react-redux';
 
 import CustomerSupportHeader from './components/CustomerSupportHeader';
 import Footer from '../../components/common/Footer';
 
-
-
-
 const AddNotice = () => {
+  const member = useSelector((state) => state.auth.member);
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const quillRef = useRef(null);
-
 
   const handleTitleChange = (e) => {
     setTitle(e.target.value);
@@ -24,49 +22,71 @@ const AddNotice = () => {
     setContent(value);
   };
 
-  const handleAddNotice = async () => {
+  const handleImageUpload = async (file) => {
+    const formData = new FormData();
+    formData.append('attachment', file);
+
     try {
-      const response = await axiosInstance.post('/customersupport/addNotice', {
-        title: title,
-        feedText: content,
+      const response = await axiosInstance.post('/customersupport/uploadImage', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      console.log(JSON.stringify(response.data));
+
+      return response.data; // 서버에서 반환한 이미지 URL
+    } catch (error) {
+      console.error('Error uploading attachment:', error);
+      return null;
+    }
+  };
+
+  const insertImage = async () => {
+
+    console.log("insertImage");
+
+    const input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+    input.click();
+
+    input.onchange = async () => {
+      const file = input.files[0];
+
+      if (file) {
+        const imageUrl = await handleImageUpload(file);
+        console.log(imageUrl);
+
+        if (imageUrl) {
+          const quill = quillRef.current.getEditor();
+          const range = quill.getSelection();
+          quill.insertEmbed(range.index, 'image', imageUrl);
+          console.log(`Inserted image URL: ${imageUrl}`);
+        }
+      }
+    };
+  };
+
+  const handleAddNotice = async () => {
+    const editorContent = quillRef.current.getEditor().root.innerHTML;
+    const formData = new FormData();
+    formData.append('writerId', member.memberId);
+  
+    formData.append('title', title);
+    formData.append('feedText', editorContent);
+
+    try {
+      const response = await axiosInstance.post('/customersupport/addNotice', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
       });
       console.log('Notice added:', response.data);
     } catch (error) {
       console.error('Error adding notice:', error);
     }
   };
-
-  const handleImageUpload = (file) => {
-    const formData = new FormData();
-    formData.append('image', file);
-
-    axiosInstance.post('/customersupport/addNotice', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    })
-      .then(response => {
-        const quill = quillRef.current.getEditor();
-        const range = quill.getSelection();
-        quill.insertEmbed(range.index, 'image', response.data.imageUrl);
-      })
-      .catch(error => {
-        console.error('Error uploading image:', error);
-      });
-  };
-
-  useEffect(() => {
-    const observer = new MutationObserver(() => {
-      // 필요한 로직 추가
-    });
-    const editor = quillRef.current.getEditor().root;
-    observer.observe(editor, { childList: true, subtree: true });
-
-    return () => {
-      observer.disconnect();
-    };
-  }, []);
-
 
   const modules = useMemo(() => {
     return {
@@ -77,25 +97,11 @@ const AddNotice = () => {
           ['bold', 'italic', 'underline', 'strike', 'blockquote'],
         ],
         handlers: {
-          image: () => {
-            const input = document.createElement('input');
-            input.setAttribute('type', 'file');
-            input.setAttribute('accept', 'image/*');
-            input.click();
-  
-            input.onchange = () => {
-              const file = input.files[0];
-              if (file) {
-                handleImageUpload(file);
-              }
-            };
-          },
+          image: insertImage,
         },
       },
     };
   }, []);
-
-
 
   const formats = [
     'header', 'font', 'size',
@@ -123,10 +129,10 @@ const AddNotice = () => {
           onChange={handleTextChange} 
           modules={modules} 
           formats={formats} 
-          style={{ height: '350px', marginBottom: '50px' }}
+          style={{ height: '300px', marginBottom: '50px' }}
         />
       </div>
-      <button onClick={handleAddNotice} style={{ padding: '10px 20px', fontSize: '16px', marginBottom: '100px',  backgroundColor: '#030722'}}>
+      <button onClick={handleAddNotice} style={{ padding: '10px 20px', fontSize: '16px', marginBottom: '100px', backgroundColor: '#030722', color: '#FFFFFF' }}>
         등록
       </button>
       <Footer/>
