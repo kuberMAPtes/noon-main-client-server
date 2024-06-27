3de96ad55c66ca5e48fc6d50a1e3acee8fe2a81f
ellen310 <ellen10735@gmail.com>
2024-06-27 17:06:20 +0900

    feat : Header

--- src/pages/CustomerSupport/GetCustomerSupport.jsx
+++ src/pages/CustomerSupport/GetCustomerSupport.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import CustomerSupportHeader from './components/CustomerSupportHeader';
 import Support from './components/Support';
 import Footer from '../../components/common/Footer';
+import Header from '../../components/common/Header';
 
 
 const GetCustomerSupport = () => {
   return (
     <div className="customer-support">
-        <CustomerSupportHeader title="고객 지원" />
+         <Header title="고객 지원"/>
         <Support />
         <Footer />
     </div>
