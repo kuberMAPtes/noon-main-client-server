61095bba9411e5c07973e1f339f3a29cc9f053a6
ellen310 <ellen10735@gmail.com>
2024-07-02 13:57:49 +0900

    design : report

--- src/pages/CustomerSupport/GetCustomerSupport.jsx
+++ src/pages/CustomerSupport/GetCustomerSupport.jsx
@@ -3,14 +3,20 @@ import CustomerSupportHeader from './components/CustomerSupportHeader';
 import Support from './components/Support';
 import Footer from '../../components/common/Footer';
 import Header from '../../components/common/Header';
+import BuildingChart from './components/BuildingChart';
+import { Row } from 'reactstrap';
 
 
 const GetCustomerSupport = () => {
   return (
     <div className="customer-support">
-         <Header title="고객 지원"/>
+        <Header title="고객 지원"/>
+
+        <Row style={{ textAlign: "center", margin: "0 auto", marginTop:'40px'}}>
+          <BuildingChart />
+        </Row>
+        
         <Support />
-        <Footer />
     </div>
   );
 };
